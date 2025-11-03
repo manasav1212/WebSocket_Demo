f1aae732b71c9bcfaf72ecc7df579accccac149a
manasav1212 <manasav@unm.edu>
2025-11-03 11:41:11 -0700

    Added skeleton of client file

--- webSocket_Protocol.js
+++ webSocket_Protocol.js
@@ -1,1 +1,22 @@
 //WebSocket Demo
+const socket = new WebSocket('ws://localhost:8080');
+
+socket.onopen = function(event)
+{
+
+}
+
+socket.onmessage = function(event)
+{
+
+}
+
+socket.onclose = function(event)
+{
+
+}
+
+function sendMessage(message)
+{
+    socket.send(message);
+}
