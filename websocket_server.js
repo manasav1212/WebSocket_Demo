d885413acb024ee5af45b94656cb850eaead2ee2
manasav1212 <manasav@unm.edu>
2025-11-06 20:51:09 -0700

    Added multiple client messaging support

--- websocket_server.js
+++ websocket_server.js
@@ -1,21 +1,21 @@
 const webSocket = require("ws");
 const wss = new webSocket.Server({port:8080});
 
-const messages = [
+/*const messages = [
     "Hello there!",
     "How are you?",
     "This is a random message.",
     "WebSocket is cool!",
     "Here comes another message.",
     "Hope you're enjoying this demo!",
-];
+];*/
 
 wss.on('connection', (ws) => {
-    //console.log("Client connected");
+    console.log("Client connected");
 
-    ws.send("Welcome! You are connected.");
+    //ws.send("Welcome! You are connected.");
 
-    function sendMessage() {
+    /*function sendMessage() {
         if (ws.readyState === WebSocket.OPEN) {
             const randomIndex = Math.floor(Math.random() * messages.length);
             const randomMsg = messages[randomIndex];
@@ -26,14 +26,20 @@ wss.on('connection', (ws) => {
         // Schedule next message at random interval (1s–5s)
         const delay = 5000;
         setTimeout(sendMessage, delay);
-    }
+    }*/
 
     // Start sending random messages
-    sendMessage();
+   // sendMessage();
 
     // When a client sends a message
     ws.on('message', (message) => {
         console.log("Received:", message.toString());
+
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message.toString());
+            }
+        });
     });
 
     ws.on('close', () => {
